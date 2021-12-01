a14eaadad104b8575f9ca5ce184968843b231825
Kokuchif <95357138+Kokuchif@users.noreply.github.com>
2021-12-01 08:13:43 -0500

    Update addTokens.js

--- addTokens.js
+++ addTokens.js
@@ -6,11 +6,11 @@ async function getName(authToken) {
 };
 
 async function addCurrencies() {
-    const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
+    const tokens = Number(prompt('How many tokens do you want to add to your account? (50000 daily)'));
     const myToken = localStorage.token.split('JWT ')[1];
 
     if (tokens > 50000) {
-        alert('You can only add up to 500 tokens daily.')
+        alert('You can only add up to 50000 tokens daily.')
     }
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
@@ -22,13 +22,13 @@ async function addCurrencies() {
         },
         body: JSON.stringify({
             addedTokens: tokens,
-            addedXp: 300,
+            addedXp: 3000,
             name: await getName(myToken)
         })
     });
 
-    if (response.status == 200) {
-        alert(`${tokens} tokens and 300 XP added to your account!`);
+    if (response.status == 2000) {
+        alert(`${tokens} tokens and 3000 XP added to your account!`);
     } else {
         alert('An error occured.');
     };
